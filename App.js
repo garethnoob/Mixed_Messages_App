e637f61a7badf9818e145c7a1bc3ae151fd4eaa0
Gareth <50303433+garethnoob@users.noreply.github.com>
2021-01-15 15:12:01 +0000

    Add forecat object and add methods to App.js

--- App.js
+++ App.js
@@ -1,1 +1,33 @@
-console.log('First App line')
+
+const forecast = () => {
+    return {
+        places : ['Nelson', 'Cwmcarn', 'Ystrad', 'Cardiff', 'Swansea'],
+        weather : ['Sun', 'Rain', 'Cloud Cover', 'Hail', 'Snow'],
+        days : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        
+        getRandomPlace() {
+            const randIx = Math.floor(Math.random()* this.places.length);
+            return this.places[randIx];
+        },
+
+        getRandomWeather() {
+            const randIx = Math.floor(Math.random()* this.weather.length);
+            return this.weather[randIx];
+        },
+
+        getRandomDay() {
+            const randIx = Math.floor(Math.random()* this.days.length);
+            return this.days[randIx];
+        }
+    }
+}
+
+let dayone = forecast();
+console.log(dayone.getRandomPlace())
+console.log(dayone.getRandomWeather())
+console.log(dayone.getRandomDay())
+
+
+
+
+
